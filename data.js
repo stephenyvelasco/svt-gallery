ae737a026e7fd596ab7a004ee5c343dc9885c725
stephenyvelasco <ma.stepheny.velasco@gmail.com>
2025-11-25 18:34:36 +0800

    updated project - with javascript

---
+++ data.js
@@ -0,0 +1,90 @@
+var members = [
+    {
+        "name": "S.Coups (에스쿱스)",
+        "position": "Leader, Hip-Hop Team Leader, Rapper, Sub-Vocalist",
+        "picture":"assets/1.jpeg"
+    },
+    {
+        "name": "Jeonghan (정한)",
+        "position": "Lead Vocalist, Visual",
+        "picture":"assets/2.jpeg"
+    },
+    {
+        "name": "Joshua (조슈아)",
+        "position": "Lead Vocalist, Visual",
+        "picture":"assets/3.jpeg"
+    },
+    {
+        "name": "Jun (준)",
+        "position": "Lead Dancer, Sub-Vocalist",
+        "picture":"assets/4.jpeg"
+    },
+    {
+        "name": "Hoshi (호시)",
+        "position": "Performance Team Leader, Main Dancer, Lead, Vocalist, Sub-Rapper",
+        "picture":"assets/5.jpeg"
+    },
+    {
+        "name": "Wonwoo (원우)",
+        "position": "Rapper, Sub-Vocalist",
+        "picture":"assets/6.jpeg"
+    },
+    {
+        "name": "Woozi (우지)",
+        "position": "Vocal Team Leader, Lead Vocalist, Producer",
+        "picture":"assets/7.jpeg"
+    },
+    {
+        "name": "The8 (디에잇)",
+        "position": "Lead Dancer, Sub-Vocalist, Sub-Rapper",
+        "picture":"assets/8.jpeg"
+    },
+    {
+        "name": "Mingyu (민규)",
+        "position": "Rapper, Sub-Vocalist, Visual, Face of the Group",
+        "picture":"assets/9.jpeg"
+    },
+    {
+        "name": "DK / Dokyeom (도겸)",
+        "position": "Main Vocalist",
+        "picture":"assets/10.jpeg"
+    },
+    {
+        "name": "Seungkwan (승관)",
+        "position": "Main Vocalist, Face of the Group",
+        "picture":"assets/11.jpeg"
+    },
+    {
+        "name": "Vernon (버논)",
+        "position": "Rapper, Sub-Vocalist, Visual, Face of the Group",
+        "picture":"assets/12.jpeg"
+    },
+    {
+        "name": "Dino (디노)",
+        "position": "Main Dancer, Sub-Vocalist, Sub-Rapper, Maknae",
+        "picture":"assets/13.jpeg"
+    }
+]
+
+var units = [
+    {
+        "unit" : "BSS Unit",
+        "debut" : "(2018; 2023–present)",
+        "picture" : "assets/bss.jpeg"
+    },
+    {
+        "unit" : "Jeonghan X Wonwoo",
+        "debut" : "(2024–present)",
+        "picture" : "assets/jxw.jpeg"
+    },
+    {
+        "unit" : "Hoshi X Woozi",
+        "debut" : "(2025–present)",
+        "picture" : "assets/hxw.jpeg"
+    },
+    {
+        "unit" : "Scoups x Mingyu",
+        "debut" : "(2025–present)",
+        "picture" : "assets/cxm.jpeg"
+    }
+]
